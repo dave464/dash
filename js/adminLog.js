8c54bb7326060b81a8109f5d2c82d0ff77995563
dave464 <davebryan.sevilla@g.batstate-u.edu.ph>
2021-10-25 14:42:10 +0800

    make it better

---
+++ js/adminLog.js
@@ -0,0 +1,32 @@
+document.getElementById("loginForm").addEventListener("submit",(event)=>{
+    event.preventDefault()
+})
+
+
+firebase.auth().onAuthStateChanged((user)=>{
+    if(user){
+        location.replace("adminhome.html")
+    }
+})
+
+
+function login(){
+    const email = document.getElementById("email").value
+    const password = document.getElementById("password").value
+    firebase.auth().signInWithEmailAndPassword(email, password)
+    .catch((error)=>{
+        document.getElementById("error").innerHTML = error.message
+    })
+}
+
+
+function forgotPass(){
+    const email = document.getElementById("email").value
+    firebase.auth().sendPasswordResetEmail(email)
+    .then(() => {
+        alert("Reset link sent to your email id")
+    })
+    .catch((error) => {
+        document.getElementById("error").innerHTML = error.message
+    });
+}
---
+++ js/firebase.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyBPfwe1EXj_CDoGLVSnougb8ntqtJC7dKg",
+  authDomain: "h2order-13d55.firebaseapp.com",
+  projectId: "h2order-13d55",
+  storageBucket: "h2order-13d55.appspot.com",
+  messagingSenderId: "1074930917295",
+  appId: "1:1074930917295:web:c5fcbeb45053ce6355d95a",
+  measurementId: "G-3Z6R95RYGC"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
