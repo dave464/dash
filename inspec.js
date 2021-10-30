472fcb90d3e99ea25fe056b4f00e57efb753a5d5
dave464 <davebryan.sevilla@g.batstate-u.edu.ph>
2021-10-30 17:55:33 +0800

    changes

--- inspec.js
+++ inspec.js
@@ -20,7 +20,7 @@ $(document).ready(function() {
     const iconClear = '<svg class="bi bi-trash" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/><path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/></svg>';
 
     var db = firebase.database();
-    var Waterstation = db.ref().child("waterstation");
+    var Waterstation = db.ref().child("inspection");
          
     
 
@@ -144,7 +144,7 @@ $('.edit').click(function(){
         if (result.value) {
             let row = $('#tableInspection').dataTable().fnGetData($(this).closest('tr'));            
             let id = row[0];            
-            db.ref(`waterstation/${id}`).remove()
+            db.ref(`inspection/${id}`).remove()
             Swal.fire('Deleted!', 'The Data has been removed.','success')
         }
         })        
